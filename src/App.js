f11bcd2add530717e05e44d7d314a8e7b4ac95df
Morfey111 <boris.marinov.marinov@gmail.com>
2021-09-17 10:31:42 +0300

    test GET i
    in class component with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-const App = () => {
-  return ( 
-  <div>
-    <h2>This is test</h2>
-  </div>
-   );
+import React, { Component } from "react"
+import PostList from "./API/PostLIst"
+import axios from "axios"
+
+
+class App extends Component {
+    constructor(props){
+      super(props)
+
+      this.state={
+        postData: []
+      }
+    }
+    componentDidMount(){
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response=>{
+        this.setState({
+          postData: response.data
+        })
+        console.log(response.data)
+      })
+    }
+
+   
+  render() {
+    return (
+      <div>
+
+        <PostList />
+      </div>
+    )
+  }
 }
- 
-export default App;
+
+export default App
