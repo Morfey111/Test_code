2561f93444638f664724b45fc4a6cf35e9fc6970
Morfey111 <boris.marinov.marinov@gmail.com>
2021-09-23 09:08:06 +0300

    request with Axios

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import React, { Component } from "react"
-import PostList from "./API/PostLIst"
-import axios from "axios"
+import axios from "axios";
 
+const App = () => {
 
-class App extends Component {
-    constructor(props){
-      super(props)
+  axios.defaults.baseURL= 'https://reqres.in/api'
 
-      this.state={
-        postData: []
-      }
+  const getData = async ()=>{
+    try{
+      const res = await axios.get('/users')
+      console.log(res.data.data)
+    } catch(err){
+      console.log(err)
     }
-     componentDidMount(){
-       axios.get('https://jsonplaceholder.typicode.com/posts')
-       .then(response=>{
-         this.setState({
-           postData: response.data
-         })
-         console.log(response.data)
-       })
-     }
-
-   
-  render() {
-    const {postData} = this.state
-    return (
-      <div>
-        <PostList />
-        {postData.map(post=>{
-          return(<div key={post.id}>{post.title}</div>)
-        })}
-      </div>
-    )
   }
-}
 
-export default App
+  const postData = async ()=>{
+    try{
+      const res = await axios.post('/users',{
+        name:'Boris Marinov',
+        job: 'Junior React developer',
+        age: 41
+      })
+      console.log(res)
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  const updateDtata = async ()=>{
+    try{
+      const res = await axios.put('/2',{
+        name: 'Peter Gorniski',
+        job : 'Senior developer',
+        age: 39
+      })
+      console.log(res)
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+
+  const deleteData = async ()=>{
+    try{
+      const res = await axios.delete('/2')
+      console.log(res.status)
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  return ( <div>
+    <h1>App component</h1>
+
+    <button onClick={getData}>Get data</button>
+    <button onClick={postData}>Post data</button>
+    <button onClick={updateDtata}>Update data</button>
+    <button onClick={deleteData}>Delete data</button>
+
+  </div> );
+}
+ 
+export default App;
